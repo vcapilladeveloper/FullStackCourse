73f6a20ec98329d6dc37ad315d9db399eab79f71
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-01 19:06:15 +0200

    Exercises 1.1 and 1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+const Header = (props) => {
+  return (<h1>{props.title}</h1>)
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.title} {props.number}
+    </p>)
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part title={props.part1} number={props.exercises1} />
+      <Part title={props.part2} number={props.exercises2} />
+      <Part title={props.part3} number={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Foundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
