daa3b51c146c6797a394ac05e911e0be0fb3c3bd
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-01 22:02:37 +0200

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,8 +5,6 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -17,6 +15,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
     </div>
   )
 }
