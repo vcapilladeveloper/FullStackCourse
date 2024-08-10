ad30c68f8ebd8624a675092eb5d2b571bfaf3aa7
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-10 12:21:46 +0200

    Exercise 2.11

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Note from "./components/Note"
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
 
@@ -23,6 +24,18 @@ const notesToShow = showAll ? notes : notes.filter(note => note.important)
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   return (
     <div>
