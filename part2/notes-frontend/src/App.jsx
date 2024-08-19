b97cdbf43611e272e1d11ef466cfdf2757c243f7
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-19 17:03:26 +0200

    Fix error for exercise 3.22

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -44,15 +44,17 @@ const App = () => {
   useEffect(hook, [])
 
   const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
+    const note = notes.find(n => n.id === `${id}`)
     const changedNote = { ...note, important: !note.important }
-
+    console.log(note)
+    console.log(`${id}`)
+    console.log(changedNote)
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        setNotes(notes.map(note => note.id !== `${id}` ? note : returnedNote))
       })
-      .catch(error => {
+      .catch(() => {
         setErrorMessage(
           `the note '${note.content}' was already deleted from server`
         )
